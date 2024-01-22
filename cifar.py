ca216f664ef8890b883af12985f112d0bd625def
George Kouretas <thegreekgeorge07@gmail.com>
2024-01-22 01:34:45 -0800

    Added better docs to cifar.py, made convention of xcorr consistent with equation from class

--- cifar.py
+++ cifar.py
@@ -28,7 +28,12 @@ class UnpackedCIFARData(TypedDict):
     data: np.ndarray
     filenames: list[str]
 
-def get_metadata():
+def get_metadata() -> dict:
+    """Returns metadata of CIFAR datasets. Will return global `_META_DATA` variable once initialized
+
+    Returns:
+        dict: Dictionary containing metadata for CIFAR datasets
+    """
     global _META_DATA
     
     # if metadata is uninitialized, unpack it
@@ -39,22 +44,64 @@ def get_metadata():
     return copy.deepcopy(_META_DATA)
 
 def get_label_names() -> list[str]:
+    """Returns label names for CIFAR datasets
+
+    Returns:
+        list[str]: List of label names
+    """
     md = get_metadata()
     return md['label_names']
 
-def label_name_to_index(label: str): return get_label_names().index(label)
-def label_index_to_name(label: int): return get_label_names()[label]
+def label_name_to_index(label: str) -> int: 
+    """Returns label index, given the corresponding string. Must be within bounds.
 
-def get_images_by_label(data: UnpackedCIFARData, label: int | str):
+    Args:
+        label (str): Label name
+
+    Returns:
+        int: Label index
+    """
+    return get_label_names().index(label)
+
+def label_index_to_name(label: int) -> str: 
+    """Returns label name, given the corresponding index. Must be valid name.
+
+    Args:
+        label (int): Label index
+
+    Returns:
+        str: Label name
+    """
+    return get_label_names()[label]
+
+def get_images_by_label(data: UnpackedCIFARData, label: int | str) -> tuple[list[np.ndarray], list[str]]:
+    """Returns images and associated filenames for a given label and dataset.
+
+    Args:
+        data (UnpackedCIFARData): Unpacked CIFAR dataset
+        label (int | str): Label, represented as integer or string
+
+    Returns:
+        tuple[list[np.ndarray], list[str]]: Length 2 tuple with image data (as 1D-array) and image filename
+    """
+    # Convert label to integer if passed as string
     if isinstance(label, str):
         label = label_name_to_index(label)
                 
-    labels = np.array(data['labels'])
-    label_indexes = np.where(labels == label)[0]
+    # Get indexes where desired label is
+    label_indexes = np.where(np.array(data['labels']) == label)[0]
     
     return [data["data"][ii] for ii in label_indexes], [data["filenames"][ii] for ii in label_indexes]
 
 def unpack(name: str) -> UnpackedCIFARData:
+    """Unpack CIFAR dataset. Unpickles data and decode byte-strings.
+
+    Args:
+        name (str): Name of dataset. Must exist in datasets/cifar-10-batches-py
+
+    Returns:
+        UnpackedCIFARData: Unpacked dataset.
+    """
     file = os.path.join(_PATH, name)    # get full path to dataset
     data = _load_pickled_file(file)     # load pickled file
         
@@ -78,11 +125,14 @@ def unpack(name: str) -> UnpackedCIFARData:
                 
     return decoded_data
             
-def data_to_rgb_image(data: np.ndarray[_IMAGE_DTYPE]): 
-    """Convert flat numpy array to RGB image
+def data_to_rgb_image(data: np.ndarray) -> np.ndarray: 
+    """Convert flat numpy array to RGB image (size=32x32x3)
 
     Args:
-        data (np.ndarray): (_description_)
+        data (np.ndarray): Input data (1D array)
+
+    Returns:
+        np.ndarray: RGB image array
     """
     assert np.shape(data) == (_NUM_BYTES,), \
         f"Invalid size of data: {np.shape(data)}"
@@ -90,12 +140,15 @@ def data_to_rgb_image(data: np.ndarray[_IMAGE_DTYPE]):
     # Initialize array 
     rgb = np.zeros(_IMAGE_SHAPE, dtype = _IMAGE_DTYPE)
     
+    # Red channel
     rgb[:, :, 0] = \
         data[:(_NUM_BYTES//3)].reshape(_IMAGE_SHAPE[:2])
         
+    # Green channel
     rgb[:, :, 1] = \
         data[(_NUM_BYTES//3):(2*_NUM_BYTES//3)].reshape(_IMAGE_SHAPE[:2])
         
+    # Blue channel
     rgb[:, :, 2] = \
         data[(2*_NUM_BYTES//3):].reshape(_IMAGE_SHAPE[:2])
         
@@ -104,6 +157,17 @@ def data_to_rgb_image(data: np.ndarray[_IMAGE_DTYPE]):
 # ----- Private functions -----
         
 def _load_pickled_file(file: str) -> dict:
+    """Helper function to load pickled file
+
+    Args:
+        file (str): File path to pickled file
+
+    Raises:
+        FileNotFoundError: Raises if file is not found at specified path
+
+    Returns:
+        dict: Unpacked data
+    """
     if os.path.exists(file):
         # unpack dataset, if it exists
         with open(file, 'rb') as fp:
@@ -111,11 +175,15 @@ def _load_pickled_file(file: str) -> dict:
     else:
         raise FileNotFoundError(f"File not found: {file}")
     
-def _unpack_metadata():
-    file = os.path.join(_PATH, "batches.meta")    
-    # unpack dataset, if it exists
-    with open(file, 'rb') as fp:
-        data: dict = pickle.load(fp, encoding = 'bytes')
+def _unpack_metadata() -> dict:
+    """Helper function for unpacking metadata. Should not be called directly, use `get_metadata` instead.
+
+    Returns:
+        dict: Unpacked meta data
+    """
+    
+    # Unpack dataset
+    data = _load_pickled_file(os.path.join(_PATH, "batches.meta"))
         
     decoded_data = {}
         
